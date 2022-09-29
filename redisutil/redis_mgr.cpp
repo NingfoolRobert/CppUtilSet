61169c5f1ba69d2019f094898b549c68f8cbbb11
Beifei Ning (IT) <Beifei.Ning@cicc.com.cn>
2022-09-29 14:08:13 +0800

    perf(release): cmake

--- redisutil/redis_mgr.cpp
+++ redisutil/redis_mgr.cpp
@@ -5,22 +5,44 @@
 CRedisMgr::CRedisMgr():_max_size(20)
 {
 }
-	
+
+//
 CRedisMgr::~CRedisMgr()
 {
 	Clear();
 }
-
+//
+bool CRedisMgr::Init(const char* ip, int port, const char* pwd, int size/* = 10*/)
+{
+	strcpy(_ip, ip);
+	strcpy(_pwd, pwd);
+	_port = port;
+	for(auto i = 0; i < size; ++i)
+	{
+		auto pConn = new CRedisUtil();
+		if(!pConn->Connect(_ip, _port, _pwd))
+		{
+			printf("Connect fail, ip:port=%s:%d", ip, port);
+			delete pConn;
+			return false;
+		}
+	}
+	return true;
+}
 
 CRedisUtil*	 CRedisMgr::GetConn()
 {
 	CRedisUtil*  pConn = NULL;
 	std::unique_lock<std::mutex> _(_lck);
-	if(!_wait_conns.empty())
+	while(!_wait_conns.empty())
 	{
 		auto pConn = _wait_conns.front();
 		_wait_conns.pop();
-		return pConn;	
+		if(pConn->Ping())
+			return pConn;	
+		delete pConn;
+		pConn == NULL;
+		_cur_size--;
 	}
 	//
 	if(_cur_size >= _max_size)
